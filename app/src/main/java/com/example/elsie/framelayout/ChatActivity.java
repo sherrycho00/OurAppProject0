38048e09cd84e9fc273c0170b4ca03182ca38a7b
Cynn <13192653862@163.com>
2017-11-14 23:01:21 +0800

    from cy

--- app/src/main/java/com/example/elsie/framelayout/ChatActivity.java
+++ app/src/main/java/com/example/elsie/framelayout/ChatActivity.java
@@ -1,9 +1,13 @@
 package com.example.elsie.framelayout;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +35,24 @@ public class ChatActivity extends AppCompatActivity {
             chatRecyclerView.setLayoutManager(layoutManager);
             adapter =new ChatAdapter(chatList);
             chatRecyclerView.setAdapter(adapter);
+
+            //悬浮按钮响应事件
+            FloatingActionButton fab =(FloatingActionButton)findViewById(R.id.add_comment);
+            fab.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(ChatActivity.this,"添加评论按钮",Toast.LENGTH_SHORT).show();
+
+//                    Snackbar.make(view,"Data deleted",Snackbar.LENGTH_SHORT)
+//                            .setAction("Undo", new View.OnClickListener() {
+//                                @Override
+//                                public void onClick(View view) {
+//                                    Toast.makeText(ChatActivity.this,"Data restored",
+//                                            Toast.LENGTH_SHORT).show();
+//                                }
+//                            });
+                }
+            });
         }
 
     private void initMsgs() {
@@ -41,9 +63,11 @@ public class ChatActivity extends AppCompatActivity {
         comment chat3=new comment("Tom","12:00","TestTest",1,0,0);
         chatList.add(chat3);
         comment chat4=new comment("Tom","12:00","TestTest",2,0,0);
-        chatList.add(chat3);
+        chatList.add(chat4);
         comment chat5=new comment("Tom","12:00","TestTest",4,0,0);
-        chatList.add(chat3);
+        chatList.add(chat5);
+        chatList.add(chat5);
+        chatList.add(chat5);
     }
 }
 
